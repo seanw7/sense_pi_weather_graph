7abb55684f81a971f7439af76cc3c2362faaf533
seanw7 <seanw@protonmail.ch>
2020-04-14 21:01:45 -0700

    Added infinite loop to collect and save temp data to weather.txt file every 5 seconds

--- collect.py
+++ collect.py
@@ -3,5 +3,9 @@ from time import sleep
 
 sense = SenseHat()
 
-
-
+while True:
+    sleep(5)
+    myfile = open('weather.txt', 'a')
+    myfile.write(sense.temp)
+    myfile.write('\n')
+    myfile.close()
